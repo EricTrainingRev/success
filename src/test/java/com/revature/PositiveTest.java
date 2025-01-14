98f9e45ec2adec2e0878f1971720a7b76dd0e3a9
EricTrainingRev <eric.suminski@revature.com>
2025-01-14 12:20:07 -0500

    adding passing test

---
+++ src/test/java/com/revature/PositiveTest.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+import org.junit.Test;
+
+public class PositiveTest {
+
+    @Test
+    public void positiveTest(){
+        System.out.println("this succeeds");
+    }
+
+}
